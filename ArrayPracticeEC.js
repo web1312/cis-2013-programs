ab026c45f54790ec5883f1900b432a3d7193dbd3
web1312 <77990865+web1312@users.noreply.github.com>
2021-04-26 13:30:20 -0500

    Add files via upload

--- ArrayPracticeEC.js
+++ ArrayPracticeEC.js
@@ -14,4 +14,13 @@ var intCount;
 for(intCount=0; intCount<arrayNames.length; intCount++){
     stringMessage = stringMessage + arrayNames[intCount] + " plays "+ arrayInstruments[intCount] + "\n";
 }
-alert(stringMessage); 
+alert(stringMessage);
+
+//Task 2
+var stringTimesTable ="";
+for(var x=1; x<=12; x++){
+    for(var y=1; y<=12; y++){
+        stringTimesTable = stringTimesTable + " " + x*y + " ";
+    }
+}
+alert(stringTimesTable);
